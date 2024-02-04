fc4554fafe0658caa9739f983c0045bb07c5295a
Stefan Pla <spla@icloud.com>
2024-02-04 18:43:49 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+    const totalBatteries = batteryBatches.reduce((accumulator, element) => accumulator + element, 0);
 
+    console.log(totalBatteries);
 // Code your solution here
